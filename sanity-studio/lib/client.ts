f9c3adbb7b3e09f9fefbcb35be372187a663c118
Nelson Onuegbu <leonenelson38@gmail.com>
2024-05-06 00:20:09 +0100

    Update to client.ts file

--- sanity-studio/lib/client.ts
+++ sanity-studio/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
 export const sanityClient = createClient({
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-29",
+  apiVersion: "2024-05-05",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: true
