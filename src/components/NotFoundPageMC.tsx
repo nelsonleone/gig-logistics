7a7717c2821520cc8a7312934498a048a883df0e
Nelson Onuegbu <leonenelson38@gmail.com>
2024-05-30 17:33:18 +0100

    update to not-found page

--- src/components/NotFoundPageMC.tsx
+++ src/components/NotFoundPageMC.tsx
@@ -10,7 +10,7 @@ export default function NotFoundPageMC(){
     return(
         <div className="flex justify-center items-center h-screen w-full">
             <MantineProvider>
-                <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+                <div className="grid grid-cols-1 gap-8 md:gap-3 md:grid-cols-2">
                     <Image src="/images/not-found-page-img.svg" alt="" aria-hidden="true" width={400} height={400} />
                     <div className="md:order-1">
                         <h1 className={`${classes.h1} ${roboto_slab.className} text-3xl my-5 font-bold`}>Something is not right...</h1>
@@ -18,7 +18,7 @@ export default function NotFoundPageMC(){
                         Page you are trying to open does not exist. You may have mistyped the address, or the
                         page has been moved to another URL. If you think this is an <span className="text-primary2">error</span> contact support.
                         </p>
-                        <Link href="/"  className={`${classes.control} bg-base-color2 p-4 text-center inline-block my-5 text-base-color1 focus:border focus:border-black focus:text-black focus:bg-transparent transition duration-300 ease-in-out hover:border hover:border-black hover:base-color2 hover:bg-transparent`}>
+                        <Link href="/"  className={`${classes.control} bg-base-color2 p-4 text-center inline-block mt-5 mb-16 text-base-color1 focus:border focus:border-black focus:text-black focus:bg-transparent transition duration-300 ease-in-out hover:border hover:border-black hover:base-color2 hover:bg-transparent`}>
                         Get back to home page
                         </Link>
                     </div>
