d398fe3518edd6cdc48065cc3ecbea95187ffb70
Nelson Onuegbu <leonenelson38@gmail.com>
2024-06-05 21:08:10 +0100

    Rolled Back Import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import CustomSkeleton from "@/components/assets/Loaders";
 import ServicesHighlight from "@/components/ServicesHighlight";
 import { Suspense } from "react"; 
 import { roboto_slab } from "./fonts";
-import TruckVideo from "../../public/videos/intro-bg-video.mp4"
  
 export default function Home() {
  
@@ -18,7 +17,7 @@ export default function Home() {
       <div 
         className="absolute w-screen h-[110vh] md:h-screen overflow-hidden brightness-[25%] top-16 bg-slate-900 after:absolute after:w-full after:h-full after:left-0 after:top-0 after:block after:opacity-10 after:m-auto after:bg-slate-200"
         > 
-        <video className="w-full absolute h-[110vh] md:h-screen top-0 bottom-0 left-0 right-0 m-auto object-cover" src={TruckVideo} autoPlay loop muted controls={false} />
+        <video className="w-full absolute h-[110vh] md:h-screen top-0 bottom-0 left-0 right-0 m-auto object-cover" src="/videos/intro-bg-video.mp4" autoPlay loop muted controls={false} />
       </div>
 
       <div className="relative w-[92.5%] lg:w-[93.5%] m-auto">
